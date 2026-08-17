215a11ee51f701068a4f7ff9fda6cb8869beea66
JyzPoohs <jessie1695@gmail.com>
2026-08-18 01:49:38 +0800

    fix: Fetch dashboard bar chart data sql date

--- tracker/src/main/java/com/expense/tracker/service/ChartService.java
+++ tracker/src/main/java/com/expense/tracker/service/ChartService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,9 +26,22 @@ public class ChartService {
     }
 
     public List<DashboardBarChartDTO> getDashboardBarChartData(Jwt jwt) {
-        LocalDateTime startDate = LocalDateTime.of(LocalDate.now().getYear(), LocalDate.now().getMonthValue(), LocalDate.now().lengthOfMonth(), 0, 0);
-        LocalDateTime endDate = LocalDateTime.of(LocalDateTime.now().getYear(), LocalDateTime.now().minusMonths(5).getMonthValue(),1, 0, 0);
+        LocalDate today = LocalDate.now();
+
+        LocalDateTime startDate = today
+                .minusMonths(5)
+                .withDayOfMonth(1)
+                .atStartOfDay();
+
+        LocalDateTime endDate = today
+                .withDayOfMonth(today.lengthOfMonth())
+                .atTime(LocalTime.MAX);
+
         List<TransactionDTO> transactions = transactionService.getByDateBetween(jwt, startDate, endDate);
+
+        for(TransactionDTO t: transactions) {
+            System.out.println("log: " + t.toString());
+        }
 
         List<DashboardBarChartDTO> dashboardBarChartData = new ArrayList<>();
 
@@ -37,6 +51,10 @@ public class ChartService {
             List<TransactionDTO> groupedTransactionsByMonth = transactions.stream()
                     .filter((transaction)-> month.equalsIgnoreCase(String.valueOf(transaction.getDate().getMonth())))
                     .toList();
+
+            for(TransactionDTO t: groupedTransactionsByMonth) {
+                System.out.println(t);
+            }
 
             BigDecimal totalExpense = groupedTransactionsByMonth.stream()
                     .filter((transaction)-> TransactionType.EXPENSE.equalsIgnoreCase(transaction.getType()))
